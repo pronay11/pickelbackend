178e42ed9aef1b4a8355580c17899b4875e22dee
pronay11 <pronaydas1122@gmail.com>
2021-10-22 17:26:56 +0600

    commit

--- achar_be/settings.py
+++ achar_be/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1oe+73j@)^+s2cna%vq=qlq6!lhcn=o57j1b&s6@s)g62a4j83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
